8bc4981a0a4a8a0da2083a37fb3547c83bfaeebc
betrayalove <partizan416@gmail.com>
2021-11-23 15:33:41 +0500

    Добавлены тесты и исправлены все замечания по PEP8

--- filter.py
+++ filter.py
@@ -1,13 +1,34 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
 
 
 class Mozabrick:
+    """ Класс, преобразующий картинку в черно-белую мозайку
+            Тест размера блока
+            >>> Mozabrick(np.array(Image.open('img2.jpg')), 10, 50).size
+            10
+
+            Тест значения градации изображения
+            >>> Mozabrick(np.array(Image.open('img2.jpg')), 10, 50).grayscale
+            5
+
+            Тест размера картинки
+            >>> Image.open('res.jpg').size
+            (750, 750)
+
+            Тест размера картинки полученного изображения
+            >>> Mozabrick(np.array(Image.open('img2.jpg')), 10, 50).convert_image().size
+            (750, 750)
+    """
 
     def __init__(self, img, size, grayscale):
         self.img = img
         self.size = size
         self.grayscale = 255 // grayscale
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testmod()
 
     def convert_image(self):
         height = len(self.img)
@@ -27,7 +48,16 @@ class Mozabrick:
 
 image = Image.open(input("Введите имя изображения: "))
 size = int(input("Введите размер мозайки: "))
+if size <= 0:
+    raise ValueError("size must be > 0")
 grayscale = int(input("Введите градацию серого: "))
+if grayscale <= 0 or grayscale > 255:
+    raise ValueError("grayscale must be > 0 and grayscale must be <= 255")
 array = np.array(image)
 result = Mozabrick(array, size, grayscale).convert_image()
 result.save(input("Введите имя для сохранения : "))
+
+# image = Image.open("img2.jpg")
+# array = np.array(image)
+# result = Mozabrick(array, size=10, grayscale=50).convert_image()
+# result.save("res.jpg")
--- old_filter.py
+++ old_filter.py
@@ -5,24 +5,24 @@ arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i < a:
     j = 0
-    while j < a1 - 11:
+    while j < a1:
         s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
+        for x in range(i, i + 10):
+            for y in range(j, j + 10):
+                n1 = arr[x][y][0]
+                n2 = arr[x][y][1]
+                n3 = arr[x][y][2]
+                M = int(n1) + int(n2) + int(n3)
                 s += M
         s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+        for x in range(i, i + 10):
+            for y in range(j, j + 10):
+                arr[x][y][0] = int(s // 50) * 50 / 3
+                arr[x][y][1] = int(s // 50) * 50 / 3
+                arr[x][y][2] = int(s // 50) * 50 / 3
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
-res.save('res.jpg')
+res.save("res1.jpg")
